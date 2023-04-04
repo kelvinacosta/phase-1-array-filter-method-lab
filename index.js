bc217cf587614467e8e5179625afc8d1732c3e1b
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-04-03 21:26:16 -0400

    complete filter fn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(driverNames,myString){
+    
+    
+    return driverNames.filter(drivers => drivers.toLowerCase() === myString.toLowerCase())
+    
+      
+}
+    
+function fuzzyMatch(driverNames,myString){
+    return driverNames.filter(drivers => drivers.charAt() === myString.charAt())
+    
+    //const myFilterDriver = driverNames.filter(function driver(driver){
+        //if(driver.charAt() === myString.charAt()){
+            //return driver
+        //}
+    //})
+    //return myFilterDriver
+}
+function matchName(driver,string){
+    return driver.filter(drivers => drivers.name === string)
+}
